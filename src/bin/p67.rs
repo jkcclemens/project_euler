216948b0c5c20178037ed90def79625e51f95bff
Kyle Clemens <jkc.clemens@gmail.com>
2017-11-05 10:58:25 -0500

    docs: remove bad comment

--- src/bin/p67.rs
+++ src/bin/p67.rs
@@ -11,8 +11,6 @@
 // Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target
 // As...'), a 15K text file containing a triangle with one-hundred rows.
 
-// DOES NOT WORK
-
 use std::cmp::max;
 use std::fs::File;
 use std::io::Read;
